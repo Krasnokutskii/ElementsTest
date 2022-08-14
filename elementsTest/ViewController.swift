cabd86793e53d35d5cf23b476f2b963114a4f8c5
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2022-08-14 20:52:05 +0300

    Initial Commit

---
+++ elementsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  elementsTest
+//
+//  Created by Yaroslav Krasnokutskiy on 14.8.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
